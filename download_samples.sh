425da923b730057757baffc0349b0db82f2a22bf
Rodrigo Garcia <absorto@sdf.org>
2014-09-04 13:28:11 -0500

    also grab family files

--- download_samples.sh
+++ download_samples.sh
@@ -16,4 +16,8 @@ for ACC in  \
     GSE4475
 do
 curl "http://www.ncbi.nlm.nih.gov/geo/download/?acc=${ACC}&format=file" > ${ACC}_RAW.tar
+wget -t 0 -c ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE43nnn/GSE43677/matrix/${ACC}_series_matrix.txt.gz
+wget -t 0 -c ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE43nnn/GSE43677/miniml/${ACC}_family.xml.tgz
+wget -t 0 -c ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE43nnn/GSE43677/soft/${ACC}_family.soft.gz
 done
+
