d2c031f8e6d28c1e1795e0dc7aecb40374dcbbea
Rodrigo Garcia <rgarcia@inmegen.gob.mx>
2014-10-28 16:24:56 -0600

    autogenerated wget script for data acquisition

---
+++ wgets.sh
@@ -0,0 +1,96 @@
+echo downloading series matrix for GSE2350
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE2nnn/GSE2350/matrix/GSE2350_series_matrix.txt.gz
+echo downloading family xml for GSE2350
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE2nnn/GSE2350/miniml/GSE2350_family.xml.tgz
+echo downloading family soft for GSE2350
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE2nnn/GSE2350/soft/GSE2350_family.soft.gz
+echo downloading series matrix for GSE4475
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE4nnn/GSE4475/matrix/GSE4475_series_matrix.txt.gz
+echo downloading family xml for GSE4475
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE4nnn/GSE4475/miniml/GSE4475_family.xml.tgz
+echo downloading family soft for GSE4475
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE4nnn/GSE4475/soft/GSE4475_family.soft.gz
+echo downloading series matrix for GSE37088
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE37nnn/GSE37088/matrix/GSE37088_series_matrix.txt.gz
+echo downloading family xml for GSE37088
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE37nnn/GSE37088/miniml/GSE37088_family.xml.tgz
+echo downloading family soft for GSE37088
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE37nnn/GSE37088/soft/GSE37088_family.soft.gz
+echo downloading series matrix for GSE55267
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE55nnn/GSE55267/matrix/GSE55267_series_matrix.txt.gz
+echo downloading family xml for GSE55267
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE55nnn/GSE55267/miniml/GSE55267_family.xml.tgz
+echo downloading family soft for GSE55267
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE55nnn/GSE55267/soft/GSE55267_family.soft.gz
+echo downloading series matrix for GSE43677
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE43nnn/GSE43677/matrix/GSE43677_series_matrix.txt.gz
+echo downloading family xml for GSE43677
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE43nnn/GSE43677/miniml/GSE43677_family.xml.tgz
+echo downloading family soft for GSE43677
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE43nnn/GSE43677/soft/GSE43677_family.soft.gz
+echo downloading series matrix for GSE26673
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE26nnn/GSE26673/matrix/GSE26673_series_matrix.txt.gz
+echo downloading family xml for GSE26673
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE26nnn/GSE26673/miniml/GSE26673_family.xml.tgz
+echo downloading family soft for GSE26673
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE26nnn/GSE26673/soft/GSE26673_family.soft.gz
+echo downloading series matrix for GSE48435
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE48nnn/GSE48435/matrix/GSE48435_series_matrix.txt.gz
+echo downloading family xml for GSE48435
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE48nnn/GSE48435/miniml/GSE48435_family.xml.tgz
+echo downloading family soft for GSE48435
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE48nnn/GSE48435/soft/GSE48435_family.soft.gz
+echo downloading series matrix for GSE42867
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE42nnn/GSE42867/matrix/GSE42867_series_matrix.txt.gz
+echo downloading family xml for GSE42867
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE42nnn/GSE42867/miniml/GSE42867_family.xml.tgz
+echo downloading family soft for GSE42867
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE42nnn/GSE42867/soft/GSE42867_family.soft.gz
+echo downloading series matrix for GSE17189
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE17nnn/GSE17189/matrix/GSE17189_series_matrix.txt.gz
+echo downloading family xml for GSE17189
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE17nnn/GSE17189/miniml/GSE17189_family.xml.tgz
+echo downloading family soft for GSE17189
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE17nnn/GSE17189/soft/GSE17189_family.soft.gz
+echo downloading series matrix for GSE17189
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE17nnn/GSE17189/matrix/GSE17189_series_matrix.txt.gz
+echo downloading family xml for GSE17189
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE17nnn/GSE17189/miniml/GSE17189_family.xml.tgz
+echo downloading family soft for GSE17189
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE17nnn/GSE17189/soft/GSE17189_family.soft.gz
+echo downloading series matrix for GSE31312
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE31nnn/GSE31312/matrix/GSE31312_series_matrix.txt.gz
+echo downloading family xml for GSE31312
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE31nnn/GSE31312/miniml/GSE31312_family.xml.tgz
+echo downloading family soft for GSE31312
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE31nnn/GSE31312/soft/GSE31312_family.soft.gz
+echo downloading series matrix for GSE57611
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE57nnn/GSE57611/matrix/GSE57611_series_matrix.txt.gz
+echo downloading family xml for GSE57611
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE57nnn/GSE57611/miniml/GSE57611_family.xml.tgz
+echo downloading family soft for GSE57611
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE57nnn/GSE57611/soft/GSE57611_family.soft.gz
+echo downloading series matrix for GSE34171
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE34nnn/GSE34171/matrix/GSE34171_series_matrix.txt.gz
+echo downloading family xml for GSE34171
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE34nnn/GSE34171/miniml/GSE34171_family.xml.tgz
+echo downloading family soft for GSE34171
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE34nnn/GSE34171/soft/GSE34171_family.soft.gz
+echo downloading series matrix for GSE12195
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE12nnn/GSE12195/matrix/GSE12195_series_matrix.txt.gz
+echo downloading family xml for GSE12195
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE12nnn/GSE12195/miniml/GSE12195_family.xml.tgz
+echo downloading family soft for GSE12195
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE12nnn/GSE12195/soft/GSE12195_family.soft.gz
+echo downloading series matrix for GSE43677
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE43nnn/GSE43677/matrix/GSE43677_series_matrix.txt.gz
+echo downloading family xml for GSE43677
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE43nnn/GSE43677/miniml/GSE43677_family.xml.tgz
+echo downloading family soft for GSE43677
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE43nnn/GSE43677/soft/GSE43677_family.soft.gz
+echo downloading series matrix for GSE12195
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE12nnn/GSE12195/matrix/GSE12195_series_matrix.txt.gz
+echo downloading family xml for GSE12195
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE12nnn/GSE12195/miniml/GSE12195_family.xml.tgz
+echo downloading family soft for GSE12195
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE12nnn/GSE12195/soft/GSE12195_family.soft.gz
