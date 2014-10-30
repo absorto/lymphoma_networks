e86c04c1b5f8e49b4bf75e8dfea0c91eb92c1414
Rodrigo Garcia <rgarcia@inmegen.gob.mx>
2014-10-30 13:19:35 -0600

    normalize per experiment per subtype

--- normalize_create_condor.py
+++ normalize_create_condor.py
@@ -5,7 +5,7 @@ print "executable     = normalize.sh"
 for subtipo in subtipos:
     for plataforma in subtipos[subtipo]:
         for experimento in subtipos[subtipo][plataforma]:
-            print "Arguments = /media/e/lymphoma_networks/data/%s %s" % (experimento, plataforma)
+            print "Arguments = /media/e/lymphoma_networks/data/%s/%s %s" % (subtipo, experimento, plataforma)
             print "output    = norm$(Process).out"
             print "error     = norm$(Process).error"
             print "universe  = vanilla"
