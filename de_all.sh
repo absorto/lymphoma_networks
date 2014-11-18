aacd30e172fae0435c04a57f26d44196e600d893
absorto <rgarcia@inmegen.gob.mx>
2014-11-18 16:05:02 -0600

    all contrasts

---
+++ de_all.sh
@@ -0,0 +1,6 @@
+Rscript de.R data/joined/burkitt_norm.tsv_controls_norm.tsv.joined 100 12 burkit_de_controls.csv burkitt_de_controls_volcano.pdf &
+Rscript de.R data/joined/burkitt_norm.tsv_dlbclgcb_norm.tsv.joined 100 236 burkitt_de_dlbclgcb.csv burkitt_de_dlbclgcb_volcano.pdf &
+Rscript de.R data/joined/burkitt_norm.tsv_follicular_norm.tsv.joined 100 71 burkitt_de_follicular.csv burkitt_de_follicular_volcano.pdf &
+Rscript de.R data/joined/controls_norm.tsv_dlbclgcb_norm.tsv.joined 12 236 controls_de_dlbclgcb.csv controls_de_dlbclgcb_volcano.pdf &
+Rscript de.R data/joined/controls_norm.tsv_follicular_norm.tsv.joined 12 71 controls_de_follicular.csv controls_de_follicular_volcano.pdf &
+Rscript de.R data/joined/dlbclgcb_norm.tsv_follicular_norm.tsv.joined 236 71 dlbclgcb_de_follicular.csv dlbclgcb_de_follicular_volcano.pdf &
