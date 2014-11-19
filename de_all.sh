a33f6abe117b27345c15b32ff3c7eef3fcab0cd3
absorto <rgarcia@inmegen.gob.mx>
2014-11-18 18:03:23 -0600

    place controls second in the three subtypes

--- de_all.sh
+++ de_all.sh
@@ -1,6 +1,6 @@
-Rscript de.R data/joined/burkitt_norm.tsv_controls_norm.tsv.joined 100 12 burkit_de_controls.csv burkitt_de_controls_volcano.pdf &
-Rscript de.R data/joined/burkitt_norm.tsv_dlbclgcb_norm.tsv.joined 100 236 burkitt_de_dlbclgcb.csv burkitt_de_dlbclgcb_volcano.pdf &
-Rscript de.R data/joined/burkitt_norm.tsv_follicular_norm.tsv.joined 100 71 burkitt_de_follicular.csv burkitt_de_follicular_volcano.pdf &
-Rscript de.R data/joined/controls_norm.tsv_dlbclgcb_norm.tsv.joined 12 236 controls_de_dlbclgcb.csv controls_de_dlbclgcb_volcano.pdf &
-Rscript de.R data/joined/controls_norm.tsv_follicular_norm.tsv.joined 12 71 controls_de_follicular.csv controls_de_follicular_volcano.pdf &
-Rscript de.R data/joined/dlbclgcb_norm.tsv_follicular_norm.tsv.joined 236 71 dlbclgcb_de_follicular.csv dlbclgcb_de_follicular_volcano.pdf &
+Rscript de.R data/joined/burkitt_controls.joined 100 12 burkitt_controls_de.csv burkitt_controls_volcano.pdf &
+Rscript de.R data/joined/dlbclgcb_controls.joined 236 12 dlbclgcb_controls_de.csv dlbclgcb_controls_volcano.pdf &
+Rscript de.R data/joined/follicular_controls.joined 71 12 follicular_controls_de.csv follicular_controls_volcano.pdf &
+Rscript de.R data/joined/burkitt_dlbclgcb.joined 100 236 burkitt_dlbclgcb_de.csv burkitt_dlbclgcb_volcano.pdf &
+Rscript de.R data/joined/burkitt_follicular.joined 100 71 burkitt_follicular_de.csv burkitt_follicular_volcano.pdf &
+Rscript de.R data/joined/dlbclgcb_follicular.joined 236 71 dlbclgcb_follicular_de.csv dlbclgcb_follicular_volcano.pdf &
