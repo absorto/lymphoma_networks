b5bfc87813ccc0b495c721f05793a0a7f20864a0
raulmejia <raulmejia@users.noreply.github.com>
2014-11-18 18:17:05 -0600

    toptable selecciona ids con lfc mayores que 1 y pvalues<0.01

--- de.R
+++ de.R
@@ -1,16 +1,17 @@
 args <- commandArgs(trailingOnly = TRUE)
+#nombre del archivo de entrada
 nomarchivo = args[1]
+#numero de columnas pertenecientes al primer dx
 A = args[2]
+#numero de columnas pertenecientes al primer dx
 B = args[3]
+print(¨¿Cómo deseas que se llame el título de tu archivo de contrastes y tu volcano plot?¨)
 salida = args[4]
 pdffile = args[5]
-
 library(affy)
 library(limma)
 library(gplots)
-
 arch=read.table(nomarchivo,header=FALSE,row.names=1)
-
 A=as.numeric(A)
 B=as.numeric(B)
 N=A+B
@@ -24,7 +25,7 @@ for(i in 1:A){
 for(i in 1:B){
   design[i+A,2]=1
   }  
-print("Tu matriz de diseno tiene la siguiente estructura:")  
+print("Tu matriz de diseño tiene la siguiente estructura:")  
 print(design)
 cont.matrix=makeContrasts("Dx1-Dx2",levels=design)
 print("Tu matriz de contrastes tiene la siguiente estructura:")
@@ -33,12 +34,12 @@ print(cont.matrix)
 fit=lmFit(arch,design) 
 fit2=contrasts.fit(fit,cont.matrix)
 fit3=eBayes(fit2)
-contrastes=("Dx1 vs Dx2")
-
+contrastes=paste("Difexp",salida,".csv",sep="")
+#contrastes=("Dx1 vs Dx2")
 pdf(pdffile)
 # La gráfica de volcán, coloreando los top 100 genes
-volcanoplot(fit3,coef=1, highlight=100, main=contrastes) 
+volcanoplot(fit3,coef=1, highlight=100, main=salida) 
 dev.off()
     
 # guarda un archivo .csv con los genes diferencialmente expresados ordenados de acuerdo al estad'isitco
-write.csv(toptable(fit3, coef=1, adjust="fdr", n=nrow(arch)), file=salida)
+write.csv(toptable(fit3, coef=1, adjust="fdr", n=nrow(arch), p.value=0.01,lfc=1), file=salida)
